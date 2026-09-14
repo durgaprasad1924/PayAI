ae383bb651ce8af1f98c98fa624fee7753b963a9
Durga prasad <porakuyya143@gmail.com>
2026-09-14 19:56:34 +0000

    Added post request to api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(title="PayAI")
+
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+
+
+users = []
 
 
 @app.get("/")
@@ -16,3 +25,16 @@ def health_check():
     return {
         "status": "healthy"
     }
+
+
+@app.post("/users")
+def create_user(user: UserCreate):
+    new_user = {
+        "id": len(users) + 1,
+        "name": user.name,
+        "email": user.email
+    }
+
+    users.append(new_user)
+
+    return new_user
